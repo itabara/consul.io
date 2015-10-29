d52c73739a4ea593cc85f18668e4cce938746ff5
Iulian Tabara <itabara@live.com>
2015-10-29 11:05:48 +0200

    Microservice discovery with consul.io

---
+++ DiscoveryMicroservice/HelloMicroservice/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microphone.Core;
+using Microphone.Core.ClusterProviders;
+using Microphone.WebApi;
+using System.Web.Http;
+
+namespace HelloMS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cluster.Bootstrap(new WebApiProvider(), new ConsulProvider(), "customers", "v1");
+            Console.ReadLine();
+        }
+    }
+
+    public class CustomerController : ApiController
+    {
+        public Customer Get()
+        {
+            return new Customer
+            {
+                CustomerID = 123,
+                FirstName = "John",
+                LastName = "Doe"
+            };
+        }
+
+        [Route("status")]
+        public string GetStatus()
+        {
+            Logger.Information("OK");
+            return "ok";
+        }
+    }
+
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string FirstName { get; set; }        
+        public string LastName { get; set; }
+    }
+}
---
+++ DiscoveryMicroservice/StandAloneClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microphone.Core;
+using Microphone.Core.ClusterProviders;
+
+namespace StandAloneClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cluster.BootstrapClient(new ConsulProvider(false));
+
+            var instance = Cluster.FindServiceInstanceAsync("customers");            
+            Console.WriteLine("Found Address:{0} Port:{1} Status:{2}", instance.Result.Address, instance.Result.Port, instance.Status);
+
+            while (true)
+            {               
+                var res = Cluster.FindServiceInstancesAsync("customers");
+                foreach(ServiceInformation serviceInstance in res.Result)
+                {                    
+                    Console.WriteLine("Found Address:{0} Port:{1}", serviceInstance.Address, serviceInstance.Port);
+                }
+                Console.ReadLine();                
+            }
+        }
+    }
+}
